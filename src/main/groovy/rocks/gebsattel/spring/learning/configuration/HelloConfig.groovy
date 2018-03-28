dc23438dd13f66213b548a3a217e859fff07d0b3
Jochen Haßfurter <jo@hassfurter.com>
2018-03-28 17:09:06 +0200

    Second steps to move annotations to the spring-config.xml. Wiring in factory beans with xml and setting primary-flag and tagging profiles in xml-notation for the spring framework

--- src/main/groovy/rocks/gebsattel/spring/learning/configuration/HelloConfig.groovy
+++ src/main/groovy/rocks/gebsattel/spring/learning/configuration/HelloConfig.groovy
@@ -1,64 +1,68 @@
-package rocks.gebsattel.spring.learning.configuration
 
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
-import org.springframework.context.annotation.Profile
-import rocks.gebsattel.spring.learning.services.HelloWorldFactory
-import rocks.gebsattel.spring.learning.services.HelloWorldService
-import rocks.gebsattel.spring.learning.services.HelloWorldServiceEnglishImpl
-import rocks.gebsattel.spring.learning.services.HelloWorldServiceSpanishImpl
-
-/*
+/* ==> Java-configuration-class moved completely to resources/spring/spring-config.xml
+ * (in the github-branch 'xml_configuration'
+ *
  * "Java-Configuration in Spring" -
  * use this configuration class instead of @Component/@Service-Annotations in the particular spring bean classes:
  * useful e.g. for imported Java-Classes, where you don't want to change code etc.
  */
 
+/* ==> moved to resources/spring/spring-config.xml:
+ * @Configuration
+ * class HelloConfig {
+ */
 
-@Configuration
-class HelloConfig {
+/* ==> moved to resources/spring/spring-config.xml:
+* @Bean
+* HelloWorldFactory helloWorldFactory(){
+* return new HelloWorldFactory()
+* }
+*/
 
-    /* ==> moved to resources/spring/spring-config.xml:
-     * @Bean
-     * HelloWorldFactory helloWorldFactory(){
-     * return new HelloWorldFactory()
-     * }
-     */
+/* ==> moved to resources/spring/spring-config.xml:
+ * @Bean
+ * @Profile("spanish")
+ * @Primary
+ * HelloWorldService helloWorldServiceSpanish(HelloWorldFactory factory) {
+ *     return factory.createHelloWorldService("es")
+ * }
+ */
 
-    @Bean
-    @Profile(["default","english"])
-    @Primary
-    HelloWorldService helloWorldServiceEnglish(HelloWorldFactory factory){
-        return factory.createHelloWorldService("en")
-    }
+/* ==> moved to resources/spring/spring-config.xml:
+ * @Bean
+ * @Profile(["default", "english"])
+ * @Primary
+ * HelloWorldService helloWorldServiceEnglish(HelloWorldFactory factory) {
+ *     return factory.createHelloWorldService("en")
+ * }
+ */
 
-    @Bean
-    @Profile("spanish")
-    @Primary
-    HelloWorldService helloWorldServiceSpanish(HelloWorldFactory factory){
-        return factory.createHelloWorldService("es")
-    }
+/* ==> moved to resources/spring/spring-config.xml:
+ * @Bean
+ * HelloWorldService helloWorldServiceRussian(HelloWorldFactory factory) {
+ *    return factory.createHelloWorldService("ru")
+ * }
+ */
 
-    @Bean
-    HelloWorldService helloWorldServiceRussian(HelloWorldFactory factory){
-        return factory.createHelloWorldService("ru")
-    }
+/* ==> moved to resources/spring/spring-config.xml:
+ * @Bean
+ * HelloWorldService helloWorldServiceGerman(HelloWorldFactory factory) {
+ *    return factory.createHelloWorldService("de")
+ * }
+ */
 
-    @Bean
-    HelloWorldService helloWorldServiceGerman(HelloWorldFactory factory) {
-        return factory.createHelloWorldService("de")
-    }
+/* ==> moved to resources/spring/spring-config.xml:
+ * @Bean
+ * HelloWorldService helloWorldServicePolish(HelloWorldFactory factory) {
+ *    return factory.createHelloWorldService("pl")
+ * }
+ */
 
-    @Bean
-    HelloWorldService helloWorldServicePolish(HelloWorldFactory factory) {
-        return factory.createHelloWorldService("pl")
-    }
-
-    @Bean(name = "french")
-    HelloWorldService helloWorldService(HelloWorldFactory factory){
-        return factory.createHelloWorldService("fr")
-    }
-
-
-}
+/* ==> moved to resources/spring/spring-config.xml:
+* @Bean(name = "french")
+* HelloWorldService helloWorldService(HelloWorldFactory factory){
+*    return factory.createHelloWorldService("fr")
+* }
+*
+* }
+*/
